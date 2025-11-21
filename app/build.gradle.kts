484c1609733b1ea1cbfae3becafaca8bc3465a81
IngLuisaGo <64036602+IngLuisaGo@users.noreply.github.com>
2025-11-21 16:05:08 -0500

    Set stable compile and target SDK versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,14 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.fitsweet"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.fitsweet"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 

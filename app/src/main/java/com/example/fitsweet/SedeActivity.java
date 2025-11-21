f0b4c00a2d8d819ba38586de104ef6a8b54e1131
IngLuisaGo <64036602+IngLuisaGo@users.noreply.github.com>
2025-11-21 17:53:46 -0500

    Embed sede map in WebView iframe

--- app/src/main/java/com/example/fitsweet/SedeActivity.java
+++ app/src/main/java/com/example/fitsweet/SedeActivity.java
@@ -9,7 +9,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SedeActivity extends AppCompatActivity {
 
-    private static final String MAP_URL = "https://www.google.com/maps?q=Valverde%20Menta%20Ch%C3%ADa%20Cundinamarca&output=embed";
+    private static final String MAP_IFRAME_HTML = "" +
+            "<!DOCTYPE html>" +
+            "<html>" +
+            "<head>" +
+            "<meta name=\"viewport\" content=\"initial-scale=1.0, width=device-width\" />" +
+            "<style>body,html{margin:0;padding:0;height:100%;} iframe{border:0;width:100%;height:100%;}</style>" +
+            "</head>" +
+            "<body>" +
+            "<iframe " +
+            "src=\"https://www.google.com/maps?q=Valverde%20Menta%20Ch%C3%ADa%20Cundinamarca&output=embed\" " +
+            "allowfullscreen loading=\"lazy\" referrerpolicy=\"no-referrer-when-downgrade\"></iframe>" +
+            "</body>" +
+            "</html>";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,6 @@ public class SedeActivity extends AppCompatActivity {
         webSettings.setDomStorageEnabled(true);
 
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl(MAP_URL);
+        webView.loadDataWithBaseURL(null, MAP_IFRAME_HTML, "text/html", "UTF-8", null);
     }
 }
